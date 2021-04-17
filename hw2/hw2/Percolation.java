8d4fd93bb9fb90434d89c9a91e3785fbf0d68de6
zhang_shizhao <shizhao.zhang01@gmail.com>
2021-04-17 10:14:17 +0800

    complete hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,96 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private final int NN;
+    private final WeightedQuickUnionUF grid;
+    private final WeightedQuickUnionUF grid2;
+    private final boolean[] openGrid;
+    private int numOfOpenSites;
+    private final int virtualTop;
+    private final int virtualBottom;
 
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("N must > 0");
+        }
+        NN = N;
+        grid = new WeightedQuickUnionUF(N * N + 2);
+        grid2 = new WeightedQuickUnionUF(N * N + 2);
+        openGrid = new boolean[N * N];
+        numOfOpenSites = 0;
+        virtualTop = N * N;
+        virtualBottom = N * N + 1;
+    }
+
+    public int rowColToN(int row, int col) {
+        return row * NN + col;
+    }
+
+    public void checkBound(int row, int col) {
+        if (row > NN || row < 0 || col > NN || col < 0) {
+            throw new java.lang.IndexOutOfBoundsException("Out of box");
+        }
+    }
+
+    public void open(int row, int col) {
+        checkBound(row, col);
+        int site = rowColToN(row, col);
+        int left = rowColToN(row, col - 1);
+        int right = rowColToN(row, col + 1);
+        int up = rowColToN(row - 1, col);
+        int down = rowColToN(row + 1, col);
+        if (col - 1 >= 0 && openGrid[left]) {
+            grid.union(left, site);
+            grid2.union(left, site);
+        }
+        if (col + 1 < NN && openGrid[right]) {
+            grid.union(right, site);
+            grid2.union(right, site);
+        }
+        if (row - 1 >= 0 && openGrid[up]) {
+            grid.union(up, site);
+            grid2.union(up, site);
+        }
+        if (row + 1 < NN && openGrid[down]) {
+            grid.union(down, site);
+            grid2.union(down, site);
+        }
+        if (!openGrid[site]) {
+            numOfOpenSites += 1;
+        }
+        openGrid[site] = true;
+        // connect to virtual top
+        if (site <= NN - 1) {
+            grid.union(site, virtualTop);
+            grid2.union(site, virtualTop);
+        }
+        // connect to virtual bottom
+        if (site >= NN * (NN - 1) && site <= NN * NN - 1) {
+            grid.union(site, virtualBottom);
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        checkBound(row, col);
+        int site = rowColToN(row, col);
+        return openGrid[site];
+    }
+
+    public boolean isFull(int row, int col) {
+        checkBound(row, col);
+        int site = rowColToN(row, col);
+        return grid2.connected(site, virtualTop);
+    }
+
+    public int numberOfOpenSites() {
+        return numOfOpenSites;
+    }
+
+    public boolean percolates() {
+        return grid.connected(virtualTop, virtualBottom);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,43 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+
+import java.util.Arrays;
 
 public class PercolationStats {
+    private final double[] fractions;
 
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        Percolation p = pf.make(N);
+        fractions = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(N - 1);
+                int col = StdRandom.uniform(N - 1);
+                p.open(row, col);
+            }
+            fractions[i] = (double) p.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    public double mean() {
+        return Arrays.stream(fractions).average().orElse(Double.NaN);
+    }
+
+    public double stddev() {
+        double mean = mean();
+        double[] squares = Arrays.stream(fractions).map(d -> (d - mean) * (d - mean)).toArray();
+        double sqSum = Arrays.stream(squares).sum();
+        return sqSum / (squares.length - 1);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(fractions.length);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(fractions.length);
+    }
 }
