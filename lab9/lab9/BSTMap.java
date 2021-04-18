2bca2dd8188cf047964e2ad494053d8affabc598
zhang_shizhao <shizhao.zhang01@gmail.com>
2021-04-18 14:33:32 +0800

    complete lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -44,7 +45,15 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        } else if (key.compareTo(p.key) == 0)  {
+            return p.value;
+        } else if (key.compareTo(p.key) > 0) {
+            return getHelper(key, p.right);
+        } else {
+            return getHelper(key, p.left);
+        }
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,14 +61,24 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
       * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            size += 1;
+            return new Node(key, value);
+        } else if (key.compareTo(p.key) == 0) {
+            p.value = value;
+        } else if (key.compareTo(p.key) > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else {
+            p.left = putHelper(key, value, p.left);
+        }
+        return p;
     }
 
     /** Inserts the key KEY
@@ -67,30 +86,190 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
 
     /* Returns a Set view of the keys contained in this map. */
+    private int height(Node p) {
+        if (p == null) {
+            return -1;
+        }
+        return 1 + Math.max(height(p.left), height(p.right));
+    }
+
+    private void levelHelper(Set<K> keys, int level, Node p) {
+        if (p == null) {
+            return;
+        } else if (level == 0) {
+            keys.add(p.key);
+            System.out.println(p.key);
+        } else {
+            levelHelper(keys, level - 1, p.left);
+            levelHelper(keys, level - 1, p.right);
+        }
+    }
+
+    private void keySetHelper(Set<K> keys, Node p) {
+        // pre-order traversal, in-order traversal, post-order traversal
+//        if (p == null) {
+//            return;
+//        }
+//        keys.add(p.key);
+//        keySetHelper(keys, p.left);
+//        keySetHelper(keys, p.right);
+
+        // level traversal
+        int h = height(root);
+        for (int i = 0; i < h + 1; i += 1) {
+            levelHelper(keys, i, root);
+        }
+    }
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keys = new HashSet<>();
+        keySetHelper(keys, root);
+        return keys;
     }
 
     /** Removes KEY from the tree if present
      *  returns VALUE removed,
      *  null on failed removal.
      */
+    private Node largest(Node p) {
+        if (p == null) {
+            return null;
+        } else if (p.right != null) {
+            return largest(p.right);
+        }
+        return p;
+    }
+
+    private Node smallest(Node p) {
+        if (p == null) {
+            return null;
+        } else if (p.left != null) {
+            return smallest(p.left);
+        }
+        return p;
+    }
+
+    private Node parent(Node p, Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == p || root.right == p || p == root) {
+            return root;
+        }
+        if (p.key.compareTo(root.key) > 0) {
+            return parent(p, root.right);
+        } else {
+            return parent(p, root.left);
+        }
+    }
+
+    private void changeBossLeft(Node curBoss, Node newBoss) {
+        // change boss
+        curBoss.key = newBoss.key;
+        curBoss.value = newBoss.value;
+        // remove newBoss's old position
+        Node parentOfNewBoss = parent(newBoss, root);
+        if (parentOfNewBoss.left == newBoss) {
+            parentOfNewBoss.left = null;
+        } else if (newBoss.left == null && newBoss.right == null) {
+            parentOfNewBoss.right = null;
+        } else if (newBoss.left != null && newBoss.right == null) {
+            newBoss.key = newBoss.left.key;
+            newBoss.value = newBoss.left.value;
+            newBoss.left = null;
+        }
+    }
+
+    private void changeBossRight(Node curBoss, Node newBoss) {
+        // change boss
+        curBoss.key = newBoss.key;
+        curBoss.value = newBoss.value;
+        // remove newBoss's old position
+        Node parentOfNewBoss = parent(newBoss, root);
+        if (parentOfNewBoss.right == newBoss) {
+            parentOfNewBoss.right = null;
+        } else if (newBoss.right == null && newBoss.left == null) {
+            parentOfNewBoss.left = null;
+        } else if (newBoss.left == null) {
+            newBoss.key = newBoss.right.key;
+            newBoss.value = newBoss.right.value;
+            newBoss.right = null;
+        }
+    }
+
+    private V removeHelper(K key, Node p) {
+        if (p == null) {
+            return null;
+        } else if (p.key.compareTo(key) == 0) {
+            Node leftLargest = largest(p.left);
+            Node rightSmallest = smallest(p.right);
+            V oldBossValue = p.value;
+            if (leftLargest != null) {
+                changeBossLeft(p, leftLargest);
+            } else if (rightSmallest != null) {
+                changeBossRight(p, rightSmallest);
+            } else {
+                Node parentOfP = parent(p, root);
+                if (parentOfP.left == p) {
+                    parentOfP.left = null;
+                } else if (parentOfP.right == p) {
+                    parentOfP.right = null;
+                } else {
+                    root = null;
+                }
+            }
+            return oldBossValue;
+        } else if (key.compareTo(p.key) > 0) {
+            return removeHelper(key, p.right);
+        } else {
+            return removeHelper(key, p.left);
+        }
+    }
+
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        return removeHelper(key, root);
+    }
+
+    private V removeHelper2(K key, V value, Node p) {
+        if (p == null) {
+            return null;
+        } else if (p.key.compareTo(key) == 0 && p.value == value) {
+            Node leftLargest = largest(p.left);
+            Node rightSmallest = smallest(p.right);
+            V oldBossValue = p.value;
+            if (leftLargest != null) {
+                changeBossLeft(p, leftLargest);
+            } else if (rightSmallest != null) {
+                changeBossRight(p, rightSmallest);
+            } else {
+                Node parentOfP = parent(p, root);
+                if (parentOfP.left == p) {
+                    parentOfP.left = null;
+                } else if (parentOfP.right == p) {
+                    parentOfP.right = null;
+                } else {
+                    root = null;
+                }
+            }
+            return oldBossValue;
+        } else if (key.compareTo(p.key) > 0) {
+            return removeHelper(key, p.right);
+        } else {
+            return removeHelper(key, p.left);
+        }
     }
 
     /** Removes the key-value entry for the specified key only if it is
@@ -99,11 +278,31 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        return removeHelper2(key, value, root);
     }
 
     @Override
     public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
     }
+
+//    public static void main(String[] args) {
+//        BSTMap<String, Integer> bstmap = new BSTMap<>();
+//        bstmap.put("d", 5);
+//        bstmap.put("b", 10);
+//        bstmap.put("f", 22);
+//        bstmap.put("a", 77);
+//        bstmap.put("c", 88);
+//        bstmap.put("e", 14);
+//        bstmap.put("g", 15);
+//        bstmap.remove("f");
+//        bstmap.remove("d");
+//        bstmap.remove("b");
+//        bstmap.remove("e");
+//        bstmap.remove("a");
+//        bstmap.remove("c");
+//        bstmap.remove("z");
+//        bstmap.remove("g");
+//        System.out.println(bstmap.keySet().toString());
+//    }
 }
--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -7,7 +8,7 @@ import java.util.Set;
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author Shizhao Zhang
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -53,19 +54,39 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        ArrayMap<K, V> bucket = buckets[hash(key)];
+        return bucket.get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        ArrayMap<K, V> bucket = buckets[hash(key)];
+        bucket.put(key, value);
+        // Resize
+        if (loadFactor() > MAX_LF) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        ArrayMap<K, V>[] oldBuckets = buckets;
+        buckets = new ArrayMap[oldBuckets.length * 2];
+        for (ArrayMap<K, V> bucket: oldBuckets) {
+            for (K k: bucket.keySet()) {
+                put(k, bucket.get(k));
+            }
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        int count = 0;
+        for (ArrayMap<K, V> bucket: buckets) {
+            count += bucket.size();
+        }
+        return count;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -73,7 +94,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> allSet = new HashSet<>();
+        for (ArrayMap<K, V> bucket: buckets) {
+            allSet.addAll(bucket.keySet());
+        }
+        return allSet;
     }
 
     /* Removes the mapping for the specified key from this map if exists.
@@ -81,7 +106,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        ArrayMap<K, V> bucket = buckets[hash(key)];
+        return bucket.remove(key);
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -89,7 +115,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        ArrayMap<K, V> bucket = buckets[hash(key)];
+        return bucket.remove(key, value);
     }
 
     @Override
